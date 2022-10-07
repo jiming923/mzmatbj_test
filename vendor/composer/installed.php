def5aca2d21ffd704bf9f9be2ea660ff2c60433b
maozhiming <maozhiming@qq.com>
2022-10-07 19:34:43 +0800

    no message

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mzmatbj/common',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'f13d768b236963180acceb2a3823a34656826914',
+        'type' => 'l',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mzmatbj/common' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'f13d768b236963180acceb2a3823a34656826914',
+            'type' => 'l',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
